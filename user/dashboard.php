d8da40bf7e9ca96f2fe51e3d710ffbe3727527a2
unknown <martinsprince126@gmail.com>
2023-08-18 15:56:54 -1000

    modified: home clock

--- user/dashboard.php
+++ user/dashboard.php
@@ -112,7 +112,7 @@ unset($_SESSION['wire_transfer'], $_SESSION['dom_transfer']);
                         </div>
 
                     </div>
-                    <div class="my-3 w-100 m-auto">
+                    <div class="my-3 w-100 mx-auto">
                         <script src="https://cdn.logwork.com/widget/clock.js"></script>
                         <a href="https://logwork.com/clock-widget/" class="clock-time" data-style="default-numeral" data-size="210" data-timezone="Africa/Lagos">Current time</a>
                     </div>
